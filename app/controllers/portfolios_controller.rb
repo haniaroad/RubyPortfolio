a4be3f67f98bfe1d165158d88f69951aed5d2889
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2019-05-18 23:00:59 -0400

    Created new portfolio functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,5 +4,25 @@ class PortfoliosController < ApplicationController
         @portfolio = Portfolio.all
     end
 
+    def new
+        @portfolio = Portfolio.new
+    end
+
+    def create
+
+        @portfolio = Portfolio.create(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+        respond_to do |format|
+            if @portfolio.save
+              format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+              format.json { render :show, status: :created, location: @blog }
+            else
+              format.html { render :new }
+              format.json { render json: @blog.errors, status: :unprocessable_entity }
+            end
+          end
+
+    end
+
 
 end
